3c2a4c3da8ca832957a63da2cef4afa7db455021
Fabiano <fabiano.pereira@seventh.com.br>
2022-06-21 20:03:24 -0300

    first commit

---
+++ app/src/main/java/com/fabianospdev/pocksocket/SocketHandler.kt
@@ -0,0 +1,24 @@
+package com.fabianospdev.pocksocket
+
+import io.socket.client.IO
+import io.socket.client.Socket
+import java.net.URISyntaxException
+
+object SocketHandler {
+
+    lateinit var mSocket: Socket
+
+    @Synchronized
+    fun setSocket(){
+        try {
+            mSocket = IO.socket("http://10.0.2.2:3000")
+        }catch (e: URISyntaxException){
+            println(e)
+        }
+    }
+
+    @Synchronized
+    fun getSocket(): Socket {
+        return mSocket
+    }
+}
